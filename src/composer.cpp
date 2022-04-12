81b5ba125f0c1e355e2aec3c1b64f6f7a6961b0d
Matěj Suchánek <kkartk322@gmail.com>
2022-04-12 20:37:12 +0200

    Fixing wierd stuff

--- src/composer.cpp
+++ src/composer.cpp
@@ -5,62 +5,62 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 Payload::Composer::Composer()
-    : m_payload_buffer()
-    , m_current_payload { nullptr, 0 }
+    : m_current_payload { nullptr, 0, 0x00 }
     , m_current_payload_gaps()
-    , m_current_payload_uuid(0x00)
+    , m_finished_payload { nullptr, 0, 0x00 }
+//, m_finished_payload_uuid(0)
 {
 }
 
-bool Payload::Composer::compose(const payload_uuid_t payload_uuid, const uint8_t* const payload_data, const size_t payload_length, const size_t payload_index, const size_t payload_total, const TransferFunction transferFunction)
+bool Payload::Composer::compose(const uint32_t payload_uuid, const uint8_t* const payload_data, const size_t payload_length, const size_t payload_index, const size_t payload_total, const TransferFunction transferFunction)
 {
     if (!payload_uuid) {
         log_e("Invalid payload_uuid, payload_uuid must be != 0");
         return false;
     }
 
+    // if (payload_uuid == m_finished_payload_uuid) {
+    //     return true;
+    // }
+
     // from_payload_index in payload, to_payload_index out interval <from_payload_index;to_payload_index]
-    const size_t from_payload_index = payload_index; 
+    const size_t from_payload_index = payload_index;
     const size_t to_payload_index = payload_index + payload_length;
 
-    if (payload_uuid != m_current_payload_uuid) {
-        log_d("Payload of the uuid %u begin", payload_uuid);
+    if (payload_uuid != m_current_payload.uuid) {
+        log_d("New payload %u begin. Current was %u", payload_uuid, m_current_payload.uuid);
 
         m_current_payload_gaps.clear();
         m_current_payload_gaps.emplace_back(0, payload_total);
 
-        m_current_payload_uuid = payload_uuid;
-
         try {
-            m_current_payload.data = std::make_unique<uint8_t[]>(payload_total); // override the old m_payload_buffer by a new one
+            m_current_payload.data = std::make_unique<uint8_t[]>(payload_total); // override the old m_current_payload by a new one
             m_current_payload.size = payload_total;
+            m_current_payload.uuid = payload_uuid;
         } catch (...) {
             log_e("Failed to allocate enough memory.");
-            m_current_payload.data = nullptr;
-            m_current_payload.size = 0;
+            m_finished_payload = payload_t { nullptr, 0, 0x00 };
+            return false;
         }
     }
 
-    if (payload_uuid == m_current_payload_uuid) {
+    if (payload_uuid == m_current_payload.uuid) {
 
         if (!m_current_payload.data || !m_current_payload.size) {
-            log_w("Invalid Payload buffer");
+            log_e("Invalid Payload buffer");
             return false;
         }
 
         if (to_payload_index > m_current_payload.size) {
             log_e("Buffer is not big enough");
-            m_current_payload.data = nullptr;
-            m_current_payload.size = 0;
-            m_current_payload_uuid = 0x00;
+            m_finished_payload = payload_t { nullptr, 0, 0x00 };
             return false;
         }
 
         log_d("Copying payload, to_index = %u, from_index = %u, length = %u", to_payload_index, from_payload_index, payload_length);
         if (!transferFunction(m_current_payload.data.get() + from_payload_index, payload_data, payload_length)) {
             log_e("Payload is not valid");
-            m_current_payload.data = nullptr;
-            m_current_payload.size = 0;
+            m_finished_payload = payload_t { nullptr, 0, 0x00 };
             return false;
         }
 
@@ -80,18 +80,17 @@ bool Payload::Composer::compose(const payload_uuid_t payload_uuid, const uint8_t
         }
 
         if (m_current_payload_gaps.size() == 0) {
-            log_d("Bytes end of the uuid %u", payload_uuid);
+            log_d("Payload %u completed", m_current_payload.uuid);
 
-            //m_payload_buffer.emplace(std::move(m_current_payload), m_current_payload.size);
-            m_payload_buffer.push_back(std::move(m_current_payload));
+            // m_finished_payload_uuid = m_current_payload.uuid;
 
-            m_current_payload.data = nullptr;
-            m_current_payload.size = 0;
-            m_current_payload_uuid = 0x00;
+            m_finished_payload = std::move(m_current_payload);
+            m_current_payload = payload_t { nullptr, 0, 0x00 };
+            return true;
         }
     }
 
-    return true;
+    return false;
 }
 
 bool Payload::Composer::composed(const size_t from_payload_index, const size_t to_payload_index)
@@ -105,22 +104,34 @@ bool Payload::Composer::composed(const size_t from_payload_index, const size_t t
     return true;
 }
 
-Payload::payload_uuid_t Payload::Composer::composing()
+uint32_t Payload::Composer::composing()
 {
-    return m_current_payload_uuid;
+    return m_current_payload.uuid;
 }
 
-size_t Payload::Composer::available()
+uint32_t Payload::Composer::available()
 {
-    return m_payload_buffer.size();
+    return m_finished_payload.uuid;
 }
 
-bool Payload::Composer::read(std::unique_ptr<uint8_t[]>& payload_out, size_t& size_out)
+bool Payload::Composer::read(std::unique_ptr<uint8_t[]>& payload_out, size_t& size_out, uint32_t& uuid_out)
 {
-    if (Payload::Composer::available()) {
-        payload_out = std::move(m_payload_buffer.front().data);
-        size_out = m_payload_buffer.front().size;
-        m_payload_buffer.pop_front();
+    if (this->available()) {
+        payload_out = std::move(m_finished_payload.data);
+        size_out = m_finished_payload.size;
+        uuid_out = m_finished_payload.uuid;
+        
+        m_finished_payload = payload_t { nullptr, 0, 0x00 };
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Payload::Composer::read()
+{
+    if (this->available()) {
+        m_finished_payload = payload_t { nullptr, 0, 0x00 };
         return true;
     } else {
         return false;
@@ -130,7 +141,7 @@ bool Payload::Composer::read(std::unique_ptr<uint8_t[]>& payload_out, size_t& si
 /////////////////////////////////////////////////////////////////////////////////
 
 // PayloadComposerFS::PayloadComposerFS()
-//     : m_payload_buffer()
+//     : m_ready_payload_buffer()
 //     , m_current_payload { nullptr }
 //     , m_current_payload_gaps()
 //     , m_current_payload_uuid(0x00)
@@ -202,8 +213,8 @@ bool Payload::Composer::read(std::unique_ptr<uint8_t[]>& payload_out, size_t& si
 //         if (m_current_payload_gaps.size() == 0) {
 //             log_d("Bytes end of the uuid %u", payload_uuid);
 
-//             //m_payload_buffer.emplace(std::move(m_current_payload), m_current_payload.size);
-//             m_payload_buffer.push(std::move(m_current_payload));
+//             //m_ready_payload_buffer.emplace(std::move(m_current_payload), m_current_payload.size);
+//             m_ready_payload_buffer.push(std::move(m_current_payload));
 
 //             m_current_payload.file = nullptr;
 //             m_current_payload.size = 0;
@@ -216,15 +227,15 @@ bool Payload::Composer::read(std::unique_ptr<uint8_t[]>& payload_out, size_t& si
 
 // size_t PayloadComposerFS::available()
 // {
-//     return m_payload_buffer.size();
+//     return m_ready_payload_buffer.size();
 // }
 
 // bool PayloadComposerFS::read(std::unique_ptr<File>& payload_out, size_t& size_out)
 // {
 //     if (PayloadComposerFS::available()) {
-//         payload_out = std::move(m_payload_buffer.front().file);
-//         size_out = m_payload_buffer.front().size;
-//         m_payload_buffer.pop();
+//         payload_out = std::move(m_ready_payload_buffer.front().file);
+//         size_out = m_ready_payload_buffer.front().size;
+//         m_ready_payload_buffer.pop();
 //         return true;
 //     } else {
 //         return false;
--- src/composer.h
+++ src/composer.h
@@ -15,20 +15,20 @@
 #include <memory>
 #include <queue>
 
-struct payload_t {
-    std::unique_ptr<uint8_t[]> data;
-    size_t size;
-};
-
 class Payload {
+
+    struct payload_t {
+        std::unique_ptr<uint8_t[]> data;
+        size_t size;
+        uint32_t uuid;
+    };
+
 public:
-    typedef uint32_t payload_uuid_t;
     typedef std::function<bool(uint8_t* destination, const uint8_t* source, size_t size)> TransferFunction;
 
     class Composer {
 
         struct gap_t {
-
             gap_t(size_t f, size_t t)
                 : from_index(f)
                 , to_index(t) {};
@@ -44,28 +44,33 @@ public:
     public:
         Composer();
 
-        //returns false on proccessing fail
-        bool compose(const payload_uuid_t payload_uuid, const uint8_t* const payload_data, const size_t payload_length, const size_t payload_index, const size_t payload_total, const TransferFunction transferFunction = defalutTransferFuntion);
+        // returns true on payload completetion
+        bool compose(const uint32_t payload_uuid, const uint8_t* const payload_data, const size_t payload_length, const size_t payload_index, const size_t payload_total, const TransferFunction transferFunction = defalutTransferFuntion);
+        // returns true if payload is composed in given range
         bool composed(const size_t from_payload_index, const size_t to_payload_index);
-        payload_uuid_t composing();
+        uint32_t composing();
 
-        //if payload is ready
-        size_t available();
+        // if payload is ready. returns uuid
+        uint32_t available();
 
-        //read the finished Bytes
-        bool read(std::unique_ptr<uint8_t[]>& payload_out, size_t& size_out);
+        // read the finished Bytes
+        bool read(std::unique_ptr<uint8_t[]>& payload_out, size_t& size_out, uint32_t& uuid_out);
+        bool read();
 
     private:
-        std::list<payload_t> m_payload_buffer;
         payload_t m_current_payload;
         std::list<gap_t> m_current_payload_gaps;
-        payload_uuid_t m_current_payload_uuid = 0;
+        payload_t m_finished_payload;
+        // uint32_t m_finished_payload_uuid;
     };
 
 public:
     Payload()
         : m_composer(nullptr)
+        , m_closed_payload_buffer()
+        , m_closed_payload_index(0)
     {
+        memset(m_closed_payload_buffer.data(), 0, m_closed_payload_buffer.size());
     }
 
     ~Payload()
@@ -78,46 +83,84 @@ public:
         return m_composer;
     }
 
-    // returns false on proccessing fail
-    inline bool compose(const payload_uuid_t payload_uuid, const uint8_t* const payload_data, const size_t payload_length, const size_t payload_index, const size_t payload_total, const TransferFunction transferFunction = defalutTransferFuntion)
+    // returns true if payload is composed
+    inline bool compose(const uint32_t payload_uuid, const uint8_t* const payload_data, const size_t payload_length, const size_t payload_index, const size_t payload_total, const TransferFunction transferFunction = defalutTransferFuntion)
+    {
+        for (size_t i = 0; i < m_closed_payload_buffer.size(); i++) {
+            if (payload_uuid == m_closed_payload_buffer[i]) {
+                log_i("composing finished payload");
+                return true;
+            }
+        }
+
+        if (!m_composer) {
+            // try {
+            m_composer = new Composer();
+            // } catch (...) {
+            //     log_e("Composer allocation ERROR");
+            //     delete m_composer;
+            //     m_composer = nullptr;
+            //     return false;
+            // }
+        }
+
+        if (m_composer->compose(payload_uuid, payload_data, payload_length, payload_index, payload_total, transferFunction)) {
+            m_closed_payload_buffer[m_closed_payload_index++] = payload_uuid;
+
+            if (m_closed_payload_index > m_closed_payload_buffer.size()) {
+                m_closed_payload_index = 0;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // returns uuid of a ready payload
+    inline uint32_t available()
     {
         if (!m_composer) {
-            try {
-                m_composer = new Composer();
-            } catch (...) {
-                log_e("Composer allocation ERROR");
-                delete m_composer;
-                m_composer = nullptr;
-                return false;
-            }
+            return 0x00;
         }
 
-        return m_composer->compose(payload_uuid, payload_data, payload_length, payload_index, payload_total, transferFunction);
+        return m_composer->available();
     }
 
-    // if payload is ready
-    inline size_t available()
+    // read the finished Bytes
+    inline bool read(std::unique_ptr<uint8_t[]>& payload_out, size_t& size_out, uint32_t& uuid, const bool keep_composer = false)
     {
-        if (m_composer) {
-            return m_composer->available();
-        } else {
-            return 0;
-        }
-    }
-
-    //read the finished Bytes
-    inline bool read(std::unique_ptr<uint8_t[]>& payload_out, size_t& size_out, const bool keep_composer = false)
-    {
-        if (m_composer) {
-            bool success = m_composer->read(payload_out, size_out);
-            if (!keep_composer && !m_composer->available()) {
-                delete m_composer;
-                m_composer = nullptr;
-            }
-            return success;
-        } else {
+        if (!m_composer) {
             return false;
         }
+
+        bool success = m_composer->read(payload_out, size_out, uuid);
+        if (!keep_composer && !m_composer->available()) {
+            delete m_composer;
+            m_composer = nullptr;
+        }
+        return success;
+    }
+
+    // read the finished Bytes
+    inline bool read(const bool keep_composer = false)
+    {
+        if (!m_composer) {
+            return false;
+        }
+
+        bool success = m_composer->read();
+        if (!keep_composer && !m_composer->available()) {
+            delete m_composer;
+            m_composer = nullptr;
+        }
+        return success;
+    }
+
+    inline void reset()
+    {
+        delete m_composer;
+        m_composer = nullptr;
     }
 
 private:
@@ -128,6 +171,9 @@ private:
     };
 
     Composer* m_composer;
+
+    std::array<uint32_t, 4> m_closed_payload_buffer;
+    size_t m_closed_payload_index;
 };
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -175,7 +221,7 @@ private:
 //         return true;
 //     };
 
-//     std::queue<payload_t> m_payload_buffer;
+//     std::queue<payload_t> m_ready_payload_buffer;
 //     payload_t m_current_payload;
 //     std::list<gap_t> m_current_payload_gaps;
 //     uint32_t m_current_payload_uuid = 0;
